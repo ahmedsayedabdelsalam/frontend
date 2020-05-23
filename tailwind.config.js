2504de942a3ab19dda0105c36b52027326567747
ahmed sayed <e.eng.ahmedsayed@gmail.com>
2020-05-23 08:33:58 +0200

    finish theme 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      lineHeight: {
+        'extra-loose': '3.7',
+      },
+      borderRadius: {
+        'xl': '1rem',
+        '2xl': '1.7rem',
+      },
+    },
     fontFamily:{
       'sans': '"Avenir Next", Helvetica, Arial, sans-serif'
-    }
+    },
+
+
   },
   variants: {},
   plugins: [],
