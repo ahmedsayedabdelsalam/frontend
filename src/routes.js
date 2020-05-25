98ab194c66be8fd633496df4114c1026bbe102fb
ahmed sayed <e.eng.ahmedsayed@gmail.com>
2020-05-25 21:03:22 +0200

    add vue router

---
+++ src/routes.js
@@ -0,0 +1,7 @@
+import Theme1 from "./components/Theme1";
+import Theme2 from "./components/Theme2";
+
+export default  [
+    { path: '/theme1', component: Theme1 },
+    { path: '/theme2', component: Theme2 }
+];
